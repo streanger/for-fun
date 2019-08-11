4aed85d36a996e2c175a60e0be61c84634d0b261
streanger <pslisz@o2.pl>
2019-08-11 02:59:50 +0200

    Add files via upload

--- shuffle_string/shuffle_string.py
+++ shuffle_string/shuffle_string.py
@@ -26,23 +26,26 @@ def simple_write(file, s):
     return True
     
     
-def shuffle_string(s):
-    '''or maybe line by line'''
+def shuffle_string(s, reverse = False):
+    '''shuffle full string(s); reverse -> False/True - randomly/reverse_direction'''
     lines = s.splitlines()
     out = []
     for line in lines:
-        out.append(' '.join([shuffle_word(item) for item in line.split()]))
+        out.append(' '.join([shuffle_word(item, reverse) for item in line.split()]))
     out = '\n'.join(out)
     
-    # out = '\n'.join([' '.join([swap_word(item) for item in line.split()]) for line in s.splitlines()])  # one-line-version
+    # out = '\n'.join([' '.join([shuffle_word(item, reverse) for item in line.split()]) for line in s.splitlines()])  # one-line-version
     return out
     
     
-def shuffle_word(s):
+def shuffle_word(s, reverse):
     '''shuffle word if item s.strip() is more than three characters'''
     if (len(s.strip()) > 3):
         center = list(s[1:-1])
-        shuffle(center)
+        if reverse:
+            center = center[::-1]
+        else:
+            shuffle(center)
         out = s[0] + ''.join(center) + s[-1]
     else:
         out = s
@@ -52,6 +55,6 @@ def shuffle_word(s):
 if __name__ == "__main__":
     script_path()
     s = simple_read('file_in.txt')
-    out = shuffle_string(s)
+    out = shuffle_string(s, True)
     print(out)
     simple_write('file_out.txt', out)
